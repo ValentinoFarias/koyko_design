3cc09325f201a917578ecca7e3e0fda1386542f0
ValentinoFarias <valentinofariascarrion@gmail.com>
2026-01-20 16:07:24 +0000

    adding home/index page first set up

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Make lottie-player clickable
+const lottiePlayer = document.querySelector('lottie-player');
+
+lottiePlayer.addEventListener('click', function() {
+  console.log('Lottie player clicked!');
+  // Add your desired action here
+});
